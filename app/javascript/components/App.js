f6953076c18dea2d02e56a082552d96df1e98e33
dustin-oz <schelldd@gmail.com>
2022-05-25 15:45:00 -0700

    Added dependencies and app.js dummy text

---
+++ app/javascript/components/App.js
@@ -0,0 +1,19 @@
+import React from "react"
+import PropTypes from "prop-types"
+import {
+  BrowserRouter as  Router,
+  Route,
+  Switch
+} from 'react-router-dom'
+
+class App extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        <h1>Joint 4ces App</h1>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App
