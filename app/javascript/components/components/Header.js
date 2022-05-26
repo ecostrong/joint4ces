fe1034ce3295e1d4dbdb003bf0e4b564a610c756
dustin-oz <schelldd@gmail.com>
2022-05-26 18:16:06 -0500

    Added footer text, footer styling, devise const's

--- app/javascript/components/components/Header.js
+++ app/javascript/components/components/Header.js
@@ -4,7 +4,18 @@ import LOGO from '../assets/joint4ces_logo.jpg'
 
 export default class Header extends Component {
   render() {
+    const {
+      logged_in,
+      current_user,
+      new_user_route,
+      sign_in_route,
+      sign_out_route
+    } = this.props
+    console.log("logged in:", logged_in)
+    console.log("current_user:", current_user)
+
     return (
+      
       <Navbar bg="light" expand="lg">
         <Container>
           <Navbar.Brand to="#home">
