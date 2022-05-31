df88d468a08f603cfa740e3940001f47a2a7390b
ecostrong <necoarmstrong@mac.com>
2022-05-31 15:35:01 -0700

    Added Business Index, App.js fetch methods,

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -15,6 +15,7 @@ import BusinessNew from "./pages/BusinessNew";
 import MyListing from "./pages/MyListing";
 import About from "./pages/About";
 import NotFound from "./pages/NotFound";
+import { Button } from "reactstrap";
 
 class App extends React.Component {
   constructor(props) {
@@ -23,13 +24,38 @@ class App extends React.Component {
       businesses: []
     }
   }
+  componentDidMount() {
+    this.indexBusinesses()
+  }
+  indexBusinesses = () => {
+    fetch("/businesses")
+    .then(response => {
+      return response.json()
+    })
+    .then(payload => {
+      this.setState({ businesses: payload })
+    })
+    .catch(errors => {
+      console.log("index errors:", errors)
+    })
+  }
+
+  handleCreateBusiness = (newBusiness) => {
+    console.log(("We did it"))
+  }
+
   render () {
+    // console.log(this.state.businesses)
     return (
       <Router>
           <Header {...this.props} />
             <Switch>
             <Route exact path="/" component={Home} />
-            <Route path="/businessindex" component={BusinessIndex} />
+            <Route
+                path="/businessindex"
+                render={(props) => <BusinessIndex businesses={this.state.businesses} />}
+                />
+            {/* <Route path="/businessindex" component={BusinessIndex} /> */}
             <Route path="/businessshow" component={BusinessShow} />
             <Route path="/businessedit" component={BusinessEdit} />
             <Route path="/businessnew" component={BusinessNew} />
@@ -38,6 +64,7 @@ class App extends React.Component {
             <Route path="/notfound" component={NotFound} />
             </Switch>
           <Footer />
+          <Button onClick={this.indexBusinesses}>Test Fetch</Button>
       </Router>
     );
   }
--- app/javascript/components/pages/BusinessNew.js
+++ app/javascript/components/pages/BusinessNew.js
@@ -1,9 +1,39 @@
 import React, { Component } from 'react'
+import { Form, FormGroup, Input, Label } from 'reactstrap'
 
 export default class BusinessNew extends Component {
+  constructor(props) {
+    super(props)
+    this.state= {
+      form: {
+        business_name: ""
+      }
+    }
+  }
+
+  handleChange = (e) => {
+    const {form} = this.state
+    form[e.target.name] = e.target.value
+    this.setState({form: form})
+  }
+  
   render() {
+    console.log(this.state.form.business_name)
     return (
-      <h1>BusinessNew</h1>
+      <>
+        <h1>BusinessNew</h1>
+        <Form>
+          <FormGroup>
+            <Label>Business Name</Label>
+            <Input 
+              type="text"
+              name="business_name"
+              onChange={this.handleChange}
+              value={this.state.form.business_name}
+            />
+          </FormGroup>
+        </Form>
+      </>
     )
   }
 }
