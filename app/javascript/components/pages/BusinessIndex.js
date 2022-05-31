2f15cb0b5c67141414da7317508dc1c5ea606c8d
ecostrong <necoarmstrong@mac.com>
2022-05-31 11:03:00 -0700

    Mod BusinessIndex,Added State object,added CRUD

--- app/javascript/components/pages/BusinessIndex.js
+++ app/javascript/components/pages/BusinessIndex.js
@@ -1,9 +1,44 @@
 import React, { Component } from 'react'
+// import { Button, Card, CardTitle, Col, Row } from 'reactstrap'
+// import { NavLink } from 'react-router-dom'
+// import { faHome, faCity } from "@fortawesome/free-solid-svg-icons"
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 export default class BusinessIndex extends Component {
   render() {
     return (
-      <h1>BusinessIndex</h1>
+      <Row className="cards">
+            {/* { this.props.businesses.map(business => { */}
+              return (
+                {/* <Col sm="4" key={ business.id }> */}
+                  {/* <Card body>
+                    <CardTitle> */}
+                      {/* <h5><FontAwesomeIcon icon={ faHome } className="fa-icon" />{ business.street }</h5>
+                      <h5><FontAwesomeIcon icon={ faCity } className="fa-icon" />{ business.city }, { business.state }</h5> */}
+                      {/* <p className="card-button">
+                        <NavLink to={`/businesses/${business.id}`}>
+                          <Button color="secondary">
+                            More Info
+                          </Button>
+                        </NavLink>
+                      </p>
+                    </CardTitle>
+                  </Card> */}
+                {/* </Col> */}
+              {/* ) */}
+            {/* })} */}
+        </Row>
     )
   }
 }
+
+      
+
+
+
+
+
+
+
+
+
