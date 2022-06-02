52be52b1ccd8228f03ca4169888907470aa96916
dustin-oz <schelldd@gmail.com>
2022-06-02 15:55:02 -0500

    Added myListing page w/ delete  & redirect

--- app/javascript/components/pages/MyListing.js
+++ app/javascript/components/pages/MyListing.js
@@ -1,9 +1,47 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Row, Col, Card, Button } from "reactstrap";
+import {NavLink} from 'react-router-dom'
 
 export default class MyListing extends Component {
   render() {
+    const {myBusiness} = this.props
     return (
-      <h1>MyListing</h1>
-    )
+      <>
+        <h3>My Business Listing</h3>
+        {myBusiness &&
+        <Card>
+          <Row className="cards">
+            {myBusiness?.map((business) => {
+              console.log(business.id);
+              return (
+                <Col sm="4" key={business.id}>
+                  <div className="my-show-card">
+                    <h5>{business.business_name}</h5>
+                    <h6>{business.business_description}</h6>
+                    <h6>{business.business_mailing_address}</h6>
+                    <h6>{business.business_email_address}</h6>
+                    <h6>{business.business_phone_number}</h6>
+                     <img src={business.business_logo}/>
+                    <h5>{business.business_url}</h5>
+                    <p className="my-card-button">
+                      <NavLink to={`/businessedit/${business.id}`}>
+                        <button color="secondary">
+                          Update Listing
+                        </button>
+                      </NavLink>
+                      <NavLink to="/businessindex">
+                        <Button onClick={() => this.props.deleteBusiness(business.id)} >
+                          Remove Listing
+                        </Button>
+                      </NavLink>
+                    </p>
+                  </div>
+                </Col>
+              );
+            })}
+          </Row>
+        </Card>}
+      </>
+    );
   }
 }
