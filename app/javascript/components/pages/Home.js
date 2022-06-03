8f7629867cfb01117c6e9d5b8849bf0c477d34f3
dustin-oz <schelldd@gmail.com>
2022-06-03 15:13:50 -0500

    initial Styling of Container 1 & 2

--- app/javascript/components/pages/Home.js
+++ app/javascript/components/pages/Home.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
 
 export default class Home extends Component {
   render() {
     return (
-      <h1>Home</h1>
+      <>
+      <Container className="container1" fluid>
+        <Row className="row1">
+          <Col className="row1-col1">
+            <h1 className="container1-h1">WELCOME TO JOINT 4CES</h1>
+            <p className="container1-p">The Home Of Veteran Businesses</p>
+          </Col>
+          <Col classNam="row1-col2">
+            <img src="https://joint4ces.com/wp-content/uploads/2021/09/Asset-52_modified4-1024x998.png" width="300px"/>
+          </Col>
+        </Row>
+      </Container>
+
+      <Container className="container2" fluid>
+        <Row className="container2-row1">
+          <Col className="container2-row1-col1">
+            <h2 className="container2-h2">THE B2B NETWORKING PLATFORM FOR VETERAN-OWNED BUSINESSES</h2>
+            <p className="container2-p">Create & Support Strategic Partnerships</p>
+          </Col>
+        </Row>
+      </Container>
+      
+      </>
     )
   }
 }
