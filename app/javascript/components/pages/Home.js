3456c53562d88eb1de18db6d34b414b112e63d1f
dustin-oz <schelldd@gmail.com>
2022-06-04 22:02:52 -0500

    Inst'd React Icons. Styled icons on home page.

--- app/javascript/components/pages/Home.js
+++ app/javascript/components/pages/Home.js
@@ -1,5 +1,12 @@
 import React, { Component } from "react";
 import { Col, Container, Row } from "react-bootstrap";
+import {
+  FaCalendarAlt,
+  FaCannabis,
+  FaListAlt,
+  FaNetworkWired,
+  FaUserAlt,
+} from "react-icons/fa";
 
 export default class Home extends Component {
   render() {
@@ -23,6 +30,7 @@ export default class Home extends Component {
         <Container className="container2" fluid>
           <Row className="container2-row1">
             <Col className="container2-row1-col1">
+              <FaCannabis className="all-icons" />
               <h2 className="container2-h2">
                 THE B2B NETWORKING PLATFORM FOR VETERAN-OWNED BUSINESSES
               </h2>
@@ -35,13 +43,16 @@ export default class Home extends Component {
         <Container className="container3" fluid>
           <Row className="container3-row1">
             <Col className="container3-row1-col1">
-              <h2>ADVERTISE, STRATEGIZE & ORGANIZE</h2>
+              <div className="divider">
+                <h2>ADVERTISE, STRATEGIZE & ORGANIZE</h2>
+              </div>
             </Col>
           </Row>
 
           <Row className="container3-row2">
             <Col className="container3-col1">
               <Col className="container3-row2-col1">
+                <FaListAlt className="four-icons" />
                 <h2>Exclusive Veteran Business Directory</h2>
                 <p>
                   All listed businesses are verified by our team and align with
@@ -49,6 +60,7 @@ export default class Home extends Component {
                 </p>
               </Col>
               <Col className="container3-row2-col2">
+                <FaNetworkWired className="four-icons" />
                 <h2>Grow Your Network</h2>
                 <p>
                   With high quality businesses who have all been highly vetted,
@@ -58,15 +70,19 @@ export default class Home extends Component {
             </Col>
             <Col>
               <Col className="container3-row2-col1">
+                <FaCalendarAlt className="four-icons" />
                 <h2>Industry & Local Events</h2>
                 <p>
-                Meet Joint 4ces at local gatherings and find connections at network events across the nation.
+                  Meet Joint 4ces at local gatherings and find connections at
+                  network events across the nation.
                 </p>
               </Col>
               <Col className="container3-row2-col2">
+                <FaUserAlt className="four-icons" />
                 <h2>Profile System</h2>
                 <p>
-                Maintain a rich profile system and connect with like-minded entrepreneurs in the network.
+                  Maintain a rich profile system and connect with like-minded
+                  entrepreneurs in the network.
                 </p>
               </Col>
             </Col>
