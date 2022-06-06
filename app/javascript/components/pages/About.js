e6afa0427e4900eeb43d2f13e97603e18abbcbaa
dustin-oz <schelldd@gmail.com>
2022-06-05 22:12:14 -0500

    Added ReactBootStrp Container & marginTop to About

--- app/javascript/components/pages/About.js
+++ app/javascript/components/pages/About.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
+import { Container } from 'react-bootstrap'
 
 export default class About extends Component {
   render() {
     return (
-      <h1>About</h1>
+      <Container fluid style={{marginTop: "126px"}}>
+        <h1>About</h1>
+      </Container>
     )
   }
 }
